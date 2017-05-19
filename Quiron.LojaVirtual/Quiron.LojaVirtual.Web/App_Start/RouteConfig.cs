d7475b32952657016aff70d07d87b7f81a770778
Fabio Lima <fabiolimma125@gmail.com>
2017-05-18 21:55:55 -0300

    Criando categorias

--- Quiron.LojaVirtual/Quiron.LojaVirtual.Web/App_Start/RouteConfig.cs
+++ Quiron.LojaVirtual/Quiron.LojaVirtual.Web/App_Start/RouteConfig.cs
@@ -1,23 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Quiron.LojaVirtual.Web
 {
     public class RouteConfig
-    {
+    {      
+       
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //1 - Inicio
+           
+            routes.MapRoute(null,
+               "",
+               new{controller = "Vitrine"
+                   , action = "ListaProdutos"
+                   , categoria = (string)null,pagina = 1 });
+
+
+            //2
+
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Produto", action = "Index", id = UrlParameter.Optional }
+                 null,
+                "Pagina{pagina}",
+                 new { controller = "Vitrine",
+                     action = "ListaProdutos",
+                     categoria = (string) null },
+                 new {pagina = @"\d+"});
+
+
+            //3
+
+
+            routes.MapRoute(
+               null,
+               "{categoria}",
+               new {controller = "Vitrine",
+               action = "ListaProdutos",
+               pagina = 1}
             );
+
+            //4
+
+
+            routes.MapRoute(
+                 null,
+                "{categoria}/Pagina{pagina}",
+                 new {controller = "Vitrine",
+                     action =  "ListaProdutos"},
+                 new { pagina = @"\d+" });
+
+            routes.MapRoute(null, "{controller}/{action}");
+
+
         }
     }
 }
--- Quiron.LojaVirtual/Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
+++ Quiron.LojaVirtual/Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -4,23 +4,39 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Quiron.LojaVirtual.Dominio.Repositorio;
+using Quiron.LojaVirtual.Web.Models;
 
 namespace Quiron.LojaVirtual.Web.Controllers
 {
     public class VitrineController : Controller
     {
         private ProdutosRepositorio _repositorio;
-        public int ProdutosPorPagina = 3;
+        public int ProdutosPorPagina = 8;
         
-        public ActionResult ListaProdutos(int pagina = 1)
+        public ViewResult ListaProdutos(string categoria,
+            int pagina = 1)
         {
             _repositorio = new ProdutosRepositorio();
-            var produtos = _repositorio.Produtos
+
+            ProdutosViewModel model = new ProdutosViewModel
+            {
+                Produtos = _repositorio.Produtos
+                .Where(p => categoria == null || p.Categoria == categoria)
             .OrderBy(p => p.Descricao)
             .Skip((pagina - 1) * ProdutosPorPagina)
-            .Take(ProdutosPorPagina);
+            .Take(ProdutosPorPagina),
+
+                Paginacao = new Paginacao
+                {
+                    PaginaAtual = pagina,
+                    ItensPorPagina = ProdutosPorPagina,
+                    ItensTotal = _repositorio.Produtos.Count()
+                },
+
+                CategoriaAtual = categoria
+            };           
             
-            return View(produtos);
+            return View(model);
         }
     }
 }
